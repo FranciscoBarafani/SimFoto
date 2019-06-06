b7eac174fd553fa4c13d0a9b61fd810c9a264353
FranciscoBarafani <franbarafani@gmail.com>
2019-06-06 12:29:30 -0300

    Commit

--- SimFoto/SimFoto/js/app.js
+++ SimFoto/SimFoto/js/app.js
@@ -1,1 +1,34 @@
-﻿
+﻿myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('');
+  }]);
+//ROUTE PROVIDER
+myApp.config(function($routeProvider) {
+$routeProvider
+
+.when('/ejercicios', {
+templateUrl : '/ejercicios.html',
+controller : 'ejercicios'
+})
+
+.when('/modolibre', {
+templateUrl : '/modolibre.html',
+controller : 'modolibre'
+})
+
+.otherwise({redirectTo: '/'});
+});
+
+//CONTROLLERS
+app.controller('index', function($scope) {
+    $scope.message = 'Hello from FirstController';
+    });
+    
+    app.controller('ejercicios', function($scope) {
+    $scope.message = 'Hello from SecondController';
+    });
+    
+    app.controller('modolibre', function($scope) {
+    $scope.message = 'Hello from ThirdController';
+    });
+    
